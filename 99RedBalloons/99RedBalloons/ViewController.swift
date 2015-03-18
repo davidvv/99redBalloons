d672983b678e3d3561f382201a8d943fcda51df1
davidvv <idavidvv@gmail.com>
2015-03-18 19:58:20 +0100

    ultimate solution. Everything works now

--- 99RedBalloons/99RedBalloons/ViewController.swift
+++ 99RedBalloons/99RedBalloons/ViewController.swift
@@ -13,12 +13,12 @@ class ViewController: UIViewController {
     @IBOutlet weak var balloonPicureImageView: UIImageView!
     
     //declaro el Array donde voy a guardar los globos
-    var myBalloons = []
+    var myBalloons:[balloon] = []
     
     //declaro una variable var = lastBalloonIndex que me va a servir para no ver el mismo globo dos veces seguidas, la indexo con valor 0, e irá cambiando al ver nuevos globos
     
     var lastBalloonIndex = 0
-
+    var randomBalloonIndex = 0
 
   
     
@@ -41,23 +41,16 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     @IBAction func nextBalloonButtonPressed(sender: UIBarButtonItem) {
-        var randomBalloonIndex = 0
+        
+        
+//        println("\(myBalloons.count)")
+        
         do {
-            randomBalloonIndex = Int(arc4random_uniform(UInt32(99)))
+        randomBalloonIndex = Int(arc4random_uniform(UInt32(99)))
         } while randomBalloonIndex == lastBalloonIndex
         
-        lastBalloonIndex = randomBalloonIndex
-        println("\(randomBalloonIndex)")
-        
-        //con el índice aleatorio obtenido, saco de myBalloons el globo correspondiente y lo utilizo para actualizar la interfaz
-        
-        // var randomBalloon = myBalloons[randomBalloonIndex]
-      
-        balloonNumberLabel.text = "Showing balloon number \(randomBalloonIndex)"
-        let randomBalloon = myBalloons[randomBalloonIndex]
-        //balloonPicureImageView.image = myBalloons[randomBalloonIndex].image
-        
-        
+        balloonNumberLabel.text = "showing balloon number \(self.myBalloons[randomBalloonIndex].number)"
+        balloonPicureImageView.image = self.myBalloons[randomBalloonIndex].image
         
         
     }
@@ -65,7 +58,7 @@ class ViewController: UIViewController {
     //función de generación de globos:
     
     func balloonGenerator(){
-        for var balloonCounter = 0; balloonCounter <= 99; balloonCounter++ {
+        for var balloonCounter = 0; balloonCounter <= 99; ++balloonCounter {
             var currentBalloon = balloon()
             currentBalloon.number = balloonCounter
             
@@ -79,9 +72,13 @@ class ViewController: UIViewController {
                 currentBalloon.image = UIImage(named: "RedBalloon3.jpg")
             default:
                 currentBalloon.image = UIImage(named: "RedBalloon4.jpg")
+            
+            }
+           // println("\(currentBalloon.number)")
+            self.myBalloons.append(currentBalloon)
                 
                 //recuerda que el nombre de los archivos es una STRING --> entre comillas!!!!!!
-            }
+            
         }
     }
 
